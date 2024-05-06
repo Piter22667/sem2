e2e2e7b481783649dfebd95c51a2b812894d8d84
Piter22667 <99553217+Piter22667@users.noreply.github.com>
2024-05-06 17:20:49 +0300

    Update mainwindowmodel.cpp

--- mainwindowmodel.cpp
+++ mainwindowmodel.cpp
@@ -1,8 +1,14 @@
 #include "mainwindowmodel.h"
 
+TestsTableViewModel *MainWindowModel::getTestsTableModel()
+{
+    return testsTableModel;
+}
+
 MainWindowModel::MainWindowModel(QObject *parent)
     : WindowModelInterface{parent}
 {
+    testsTableModel = new TestsTableViewModel(tests, parent);
 
 }
 
@@ -14,4 +20,27 @@ QString MainWindowModel::getFirstButtonTitle()
 QString MainWindowModel::getSecondButtonTitle()
 {
     return "Pass";
+}
+
+void MainWindowModel::onDataReceived()
+{
+    DataUpdatedIvent event;
+    event.type = DataUpdatedIvent::EventType::testListReceived;
+    emit dataUpdatedWith(event);
+}
+
+void MainWindowModel::getDataSource()
+{
+    TestModel* test = new TestModel;
+    test->name = "First test";
+    Question question;
+    question.questionText = "New question 1:";
+    question.answers.push_back("Answer 1");
+    question.answers.push_back("Answer 2");
+    question.answers.push_back("Answer 3");
+    question.correctAnswer = 2;
+    test->questions.push_back(question);
+    tests.push_back(test);
+
+    onDataReceived();
 }
