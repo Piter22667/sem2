969fba69f0408d09aabf4de1a3d0b3c92386ba1f
Piter22667 <99553217+Piter22667@users.noreply.github.com>
2024-05-09 20:22:14 +0300

    Update teststableviewmodel.cpp

--- teststableviewmodel.cpp
+++ teststableviewmodel.cpp
@@ -1,9 +1,8 @@
 #include "teststableviewmodel.h"
 
 
-TestsTableViewModel::TestsTableViewModel(const QVector<TestModel*> &tests, QObject *parent)
-    : tests(tests)
-    ,QAbstractTableModel{parent}
+TestsTableViewModel::TestsTableViewModel(QObject *parent)
+    : QAbstractTableModel{parent}
 {}
 
 int TestsTableViewModel::rowCount(const QModelIndex &parent) const
@@ -41,10 +40,33 @@ QVariant TestsTableViewModel::data(const QModelIndex &index, int role) const
             return QVariant();
         }
     }
+    return QVariant();
 }
 
-void TestsTableViewModel::testsDataUpdated()
+void TestsTableViewModel::testsDataUpdated( QVector<TestModel*> tests)
 {
     beginResetModel();
+    this->tests = tests;
     endResetModel();
+}
+
+QVariant TestsTableViewModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+
+    if(role == Qt::DisplayRole){
+        switch (section){
+        case 0:
+            return "Test Name";
+
+        case 1:
+            return "Question number";
+
+        case 2:
+            return "Min test score";
+
+        default:
+            return QVariant();
+        }
+    }
+    return QVariant();
 }
