ea0cefa1274e57809c8c06ea44b5587f9f4de63c
Piter22667 <99553217+Piter22667@users.noreply.github.com>
2024-05-14 21:29:55 +0300

    Update topmainwindowwidget.cpp

--- topmainwindowwidget.cpp
+++ topmainwindowwidget.cpp
@@ -30,11 +30,17 @@ void TopMainWindowWidget::configureWidgets() {
 void TopMainWindowWidget::connectWidgets() {
 
     connect(tableView, &QTableView::doubleClicked, this, &TopMainWindowWidget::onTableViewDoubleClicked);
+    connect(tableView,&QTableView::clicked, this, &TopMainWindowWidget::onTableViewClicked);
 }
 
 void TopMainWindowWidget::onTableViewDoubleClicked(const QModelIndex &index)
 {
     emit tableViewDoubleClicked(index);
+}
+
+void TopMainWindowWidget::onTableViewClicked(const QModelIndex &index)
+{
+    emit tableViewClicked(index);
 }
 
 void TopMainWindowWidget::updateUi()
