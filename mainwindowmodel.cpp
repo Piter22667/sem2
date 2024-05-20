913df50088b93682b11003393f76eab491e9b52f
Piter22667 <99553217+Piter22667@users.noreply.github.com>
2024-05-20 17:01:39 +0300

    Update mainwindowmodel.cpp

--- mainwindowmodel.cpp
+++ mainwindowmodel.cpp
@@ -1,4 +1,7 @@
 #include "mainwindowmodel.h"
+#include "networkmanager.h"
+
+#include <QJsonDocument>
 
 TestsTableViewModel* MainWindowModel::getTestsTableModel()
 {
@@ -9,6 +12,7 @@ MainWindowModel::MainWindowModel(QObject *parent)
     : WindowModelInterface{parent}
 {
     testsTableModel = new TestsTableViewModel(this);
+    connect(NetworkManager::instance(), &NetworkManager::replyReceived, this, &MainWindowModel::onDataReceived);
 
 }
 
@@ -22,12 +26,32 @@ QString MainWindowModel::getSecondButtonTitle()
     return "Pass";
 }
 
-void MainWindowModel::onDataReceived()
+void MainWindowModel::onDataReceived(const QJsonDocument &reply)
 {
+    parseTests(reply);
+
     testsTableModel->testsDataUpdated(tests);
     DataUpdatedIvent event;
     event.type = DataUpdatedIvent::EventType::testListReceived;
     emit dataUpdatedWith(event);
+}
+
+void MainWindowModel::parseTests(const QJsonDocument &reply)
+{
+    if(reply.isNull()){
+        qWarning("No tests avaliable");
+    }
+
+    if(reply.isArray()){
+        auto testsJsonArray = reply.array();
+        for(auto const &testJsonValue:testsJsonArray){
+            auto testJsonObject = testJsonValue.toObject();
+
+            TestModel* test = new TestModel;
+            test->fromJsonObject(testJsonObject);
+            tests.append(test);
+        }
+    }
 }
 
 int MainWindowModel::getSelectedTestNumber() const
@@ -42,40 +66,41 @@ void MainWindowModel::setSelectedTestNumber(int newSelectedTestNumber)
 
 void MainWindowModel::getDataSource()
 {
+    NetworkManager::instance()->getRequest("tests");
 
-    TestModel* test = new TestModel;
-    test->minTestScore = "60";
-    test->name = "First test";
-    Question question;
-    question.questionText = " Question 1:";
-    question.answers.push_back("Answer 1");
-    question.answers.push_back("Answer 2");
-    question.answers.push_back("Answer 3");
-    question.correctAnswer = 2;
-    test->questions.push_back(question);
+    // TestModel* test = new TestModel;
+    // test->minTestScore = "60";
+    // test->name = "First test";
+    // Question question;
+    // question.questionText = " Question 1:";
+    // question.answers.push_back("Answer 1");
+    // question.answers.push_back("Answer 2");
+    // question.answers.push_back("Answer 3");
+    // question.correctAnswer = 2;
+    // test->questions.push_back(question);
 
-    Question question1;
-    question1.questionText = "New question 1:";
-    question1.answers.push_back(" New Answer 1");
-    question1.answers.push_back("New Answer 2");
-    question1.answers.push_back("Updated Answer 3");
-    question1.correctAnswer = 2;
-    test->questions.push_back(question1);
-    tests.push_back(test);
+    // Question question1;
+    // question1.questionText = "New question 1:";
+    // question1.answers.push_back(" New Answer 1");
+    // question1.answers.push_back("New Answer 2");
+    // question1.answers.push_back("Updated Answer 3");
+    // question1.correctAnswer = 2;
+    // test->questions.push_back(question1);
+    // tests.push_back(test);
 
-    TestModel* secondTest = new TestModel;
-    secondTest->minTestScore = "60";
-    secondTest->name = "Second test";
-    Question question3;
-    question3.questionText = "2 Question :";
-    question3.answers.push_back("2 Answer ");
-    question3.answers.push_back("2 Answer ");
-    question3.answers.push_back("2 Answer ");
-    question.correctAnswer = 2;
-    secondTest->questions.push_back(question3);
+    // TestModel* secondTest = new TestModel;
+    // secondTest->minTestScore = "60";
+    // secondTest->name = "Second test";
+    // Question question3;
+    // question3.questionText = "2 Question :";
+    // question3.answers.push_back("2 Answer ");
+    // question3.answers.push_back("2 Answer ");
+    // question3.answers.push_back("2 Answer ");
+    // question.correctAnswer = 2;
+    // secondTest->questions.push_back(question3);
 
-    tests.push_back(secondTest);
-    onDataReceived();
+    // tests.push_back(secondTest);
+    // onDataReceived();
 }
 
 TestModel* MainWindowModel::getTest()
