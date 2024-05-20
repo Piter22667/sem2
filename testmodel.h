6df7ffc52ce45f293ae16b47cf7a5ceaad0ac371
Piter22667 <99553217+Piter22667@users.noreply.github.com>
2024-05-20 16:58:53 +0300

    adding database processing logic

--- testmodel.h
+++ testmodel.h
@@ -3,21 +3,52 @@
 
 #include <QString>
 #include <QVector>
+
+#include <QJsonObject>
+#include <QJsonArray>
+
 struct Question{
     QString questionText;
     QStringList answers;
     int correctAnswer;
+
+    void fromJsonObject(const QJsonObject &jsonObj){
+        questionText = jsonObj["question"].toString();
+        correctAnswer = jsonObj["correct_answer"].toInt();
+
+        QJsonArray answersArray = jsonObj["options"].toArray();
+        for(const auto &answerValue:answersArray){
+            QString answer = answerValue.toString();
+            answers.append(answer);
+        }
+
+    }
 };
 
 
 struct TestModel
 {
     QString name;
-    int id;
+    QString id;
     QString minTestScore;
 
     QString description;
     QVector<Question> questions;
+
+    void fromJsonObject(const QJsonObject &jsonObj){
+        name = jsonObj["name"].toString();
+        id = jsonObj["id"].toString();
+        minTestScore = jsonObj["minTestScore"].toString();
+        description = jsonObj["description"].toString();
+
+        QJsonArray questionsArray = jsonObj["questions"].toArray();
+        for(const auto &questionValue:questionsArray){
+            QJsonObject questionObject = questionValue.toObject();
+            Question question;
+            question.fromJsonObject(questionObject);
+            questions.append(question);
+        }
+    }
 };
 
 #endif // TESTMODEL_H
