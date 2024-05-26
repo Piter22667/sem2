357de9ad8e7ad9ab32b2ed2bd3e254658546797b
Piter22667 <99553217+Piter22667@users.noreply.github.com>
2024-05-26 17:35:32 +0300

    Update testwindowmodel.h

--- testwindowmodel.h
+++ testwindowmodel.h
@@ -5,13 +5,14 @@
 #include "windowmodelinterface.h"
 #include "twobuttonsmodelinterface.h"
 #include "testmodel.h"
-#include <QVector>
+
+#include <QButtonGroup>
 
 class TestWindowModel : public WindowModelInterface, public TwoButtonsModelInterface
 {
     Q_OBJECT
 public:
-    explicit TestWindowModel(QObject *parent = nullptr);
+    explicit TestWindowModel(TestModel* test, QObject *parent = nullptr);
 
     QString getQuestion();
     QStringList getAnswers();
@@ -20,16 +21,28 @@ public:
 
     void nextButtonClicked(int id);
     void previousButtonClicked();
+    void processingAnswers();
+    int getCurrentAnswerNumber();
+
 
     QString getFirstButtonTitle() override;
     QString getSecondButtonTitle() override;
 
+    void onRadioButtonClicked(int id);
+
+
+    QString getCheckedResult() const;
+
 signals:
+
+private slots:
 
 private:
     TestModel* test = nullptr;
     int currentQuestion;
     QVector<int> chosenAnswers;
+    int checkedMark = 0;
+
 };
 
 #endif // TESTWINDOWMODEL_H
