8c4312f17060aefbe29846f01bc03b9d94dff087
Piter22667 <99553217+Piter22667@users.noreply.github.com>
2024-05-26 17:41:14 +0300

    Update toptestwidget.cpp

--- toptestwidget.cpp
+++ toptestwidget.cpp
@@ -19,12 +19,14 @@ void TopTestWidget::configureLayaut()
 void TopTestWidget::addWidgets()
 {
     QFormLayout* formLayout = new QFormLayout(this);
+    group = new QButtonGroup(this);
     questionLabel = new QLabel(model.getQuestion());
     formLayout->addRow("Question: ", questionLabel);
     QStringList answers = model.getAnswers();
     for(int i=0; i < answers.size(); i++ ){
         auto button = new QRadioButton(this);
         radioButtons.push_back(button);
+        group->addButton(button, i);
         auto label = new QLabel(answers.at(i) , this);
        labels.push_back(label);
 
@@ -47,7 +49,14 @@ void TopTestWidget::configureWidgets()
 
 void TopTestWidget::connectWidgets()
 {
+    connect(group, &QButtonGroup::idClicked, this, &TopTestWidget::onRadioButtonClicked);
 
+}
+
+void TopTestWidget::onRadioButtonClicked(int id)
+{
+    qDebug() << "Radiobutton clicked " << id;
+    emit radioButtonClicked(id);
 }
 
 void TopTestWidget::updateUi()
@@ -55,8 +64,7 @@ void TopTestWidget::updateUi()
     questionLabel->setText(model.getQuestion());
     QStringList answers = model.getAnswers();
     for(int i=0; i < answers.size(); i++ ){
-        radioButtons[i]->setChecked(false);
+        radioButtons[i]->setChecked(model.getCurrentAnswerNumber() == i);
         labels[i]->setText(answers.at(i));
     }
-
 }
